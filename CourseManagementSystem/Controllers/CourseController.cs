2c7ff7e0d7463539ea1a2d820993706a2123b471
raneem-sh7 <ranfobusiness@gmail.com>
2026-07-24 19:38:54 +0300

    Connect CourseController to DbContext

---
+++ CourseManagementSystem/Controllers/CourseController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CourseManagementSystem.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace CourseManagementSystem.Controllers
+{
+    public class CourseController : Controller
+    {
+        private readonly CourseManagmentDbContext _context;
+
+        public CourseController(CourseManagmentDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
